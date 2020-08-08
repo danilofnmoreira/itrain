343d55e12c0e87e9a6b9776f8f301c352ed0ea88
Danilo F. N. Moreira <danilofnmoreira@gmail.com>
2020-08-08 00:17:28 -0300

    client serivice

--- ms-itrain-client/src/main/java/com/itrain/client/service/ClientService.java
+++ ms-itrain-client/src/main/java/com/itrain/client/service/ClientService.java
@@ -1,11 +1,16 @@
 package com.itrain.client.service;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Optional;
 
+import com.itrain.client.domain.Address;
 import com.itrain.client.domain.Client;
+import com.itrain.client.domain.Contact;
 import com.itrain.client.repository.ClientRepository;
+import com.itrain.common.exception.DuplicateEntityException;
 
 import org.springframework.stereotype.Service;
 
@@ -35,6 +40,25 @@ public class ClientService {
     public Optional<Client> findOptionalById(final Long id) {
 
         return clientRepository.findById(id);
+    }
+
+    public Client create(final Client client) {
+
+        if (clientRepository.existsById(client.getId())) {
+            throw new DuplicateEntityException(String.format("Client, %s, already exists.", client.getId()));
+        }
+
+        final var addresses = Objects.requireNonNullElse(client.getAddresses(), new HashSet<Address>());
+        addresses.forEach(a -> a.setId(null));
+
+        final var contacts = Objects.requireNonNullElse(client.getContacts(), new HashSet<Contact>());
+        contacts.forEach(c -> c.setId(null));
+
+        client.addAddresses(addresses);
+
+        client.addContacts(contacts);
+
+        return save(client);
     }
 
 }
